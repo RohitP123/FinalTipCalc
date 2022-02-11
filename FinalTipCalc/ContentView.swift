5debbfaae05f185938cb863d38a29b1beffe661f
RohitP123 <90308603@ep-student.org>
2022-02-11 11:17:30 -0600

    Initial Commit

---
+++ FinalTipCalc/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FinalTipCalc
+//
+//  Created by 90308603 on 2/11/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ FinalTipCalc/FinalTipCalcApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinalTipCalcApp.swift
+//  FinalTipCalc
+//
+//  Created by 90308603 on 2/11/22.
+//
+
+import SwiftUI
+
+@main
+struct FinalTipCalcApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
